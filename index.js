17965e5d689f1212430dce87580ec02af6c6a416
Mehwishnaz <160154061+Mehwishnaz1603@users.noreply.github.com>
2024-04-01 23:33:42 +0500

    task no 11

---
+++ index.js
@@ -0,0 +1,8 @@
+//Task # 11 //
+//Names: Store the names of a few of your friends in a array called names. Print each person’s 
+//name by accessing each element in the list, one at a time. //
+let members = ['sana', 'ayesha', 'nimra', 'kinza'];
+for (let i = 0; i < members.length; i++) {
+    console.log(members[i]);
+}
+export {};
---
+++ index.ts
@@ -0,0 +1,9 @@
+//Task # 11 //
+//Names: Store the names of a few of your friends in a array called names. Print each person’s 
+//name by accessing each element in the list, one at a time. //
+
+
+let members: string[]= ['sana','ayesha','nimra','kinza'];
+for(let i=0; i < members.length; i++){
+        console.log(members[i]);
+}
